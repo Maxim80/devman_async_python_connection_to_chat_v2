c6369bdda4d1b2c903c887a5964c47afb53fbcb4
Maxim <maxim19802104@gmail.com>
2021-09-24 16:27:30 +0300

    Added async function read_msgs() - reads messages from the chat.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import gui
 import time
+import argparse
 
 
 async def generate_msgs(queue):
@@ -9,16 +10,34 @@ async def generate_msgs(queue):
         await asyncio.sleep(0)
 
 
-async def main():
+async def read_msgs(host, port, queue):
+    try:
+        reader, writer = await asyncio.open_connection(host, port)
+        while True:
+            msg = await reader.readline()
+            queue.put_nowait(msg.decode())
+
+    finally:
+        writer.close()
+        await writer.wait_closed()
+
+
+async def main(host, read_port, send_port):
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
 
     await asyncio.gather(
-        generate_msgs(messages_queue),
+        read_msgs(host, read_port, messages_queue),
         gui.draw(messages_queue, sending_queue, status_updates_queue)
     )
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', type=str, default='minechat.dvmn.org', help='IP or URL hosts address for connection.')
+    parser.add_argument('--read_port', type=int, default=5000, help='Port to read.')
+    parser.add_argument('--send_port', type=int, default=5050, help='Port to send.')
+    args = parser.parse_args()
+
+    asyncio.run(main(args.host, args.read_port, args.send_port))
