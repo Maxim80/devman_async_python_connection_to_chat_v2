b986e30bef371d080f22f2bb7282e86190e338c9
Maxim <maxim19802104@gmail.com>
2021-09-24 17:12:35 +0300

    Added async function save_messages(), messages history is saved to a file.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import aiofiles
 import gui
 import time
 import argparse
@@ -10,25 +11,35 @@ async def generate_msgs(queue):
         await asyncio.sleep(0)
 
 
-async def read_msgs(host, port, queue):
+async def read_msgs(host, port, messages_queue, save_queue):
     try:
         reader, writer = await asyncio.open_connection(host, port)
         while True:
             msg = await reader.readline()
-            queue.put_nowait(msg.decode())
+            messages_queue.put_nowait(msg.decode())
+            save_queue.put_nowait(msg.decode())
 
     finally:
         writer.close()
         await writer.wait_closed()
 
 
-async def main(host, read_port, send_port):
+async def save_messages(filepath, queue):
+    while True:
+        msg = await queue.get()
+        async with aiofiles.open(filepath, 'a') as f:
+            await f.write(msg)
+
+
+async def main(host, read_port, send_port, history_filepath):
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
+    save_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
 
     await asyncio.gather(
-        read_msgs(host, read_port, messages_queue),
+        read_msgs(host, read_port, messages_queue, save_queue),
+        save_messages(history_filepath, save_queue),
         gui.draw(messages_queue, sending_queue, status_updates_queue)
     )
 
@@ -38,6 +49,8 @@ if __name__ == '__main__':
     parser.add_argument('--host', type=str, default='minechat.dvmn.org', help='IP or URL hosts address for connection.')
     parser.add_argument('--read_port', type=int, default=5000, help='Port to read.')
     parser.add_argument('--send_port', type=int, default=5050, help='Port to send.')
+    parser.add_argument('--history', type=str, default='./minechat.history',
+        help='The path to the file with the message history.')
     args = parser.parse_args()
 
-    asyncio.run(main(args.host, args.read_port, args.send_port))
+    asyncio.run(main(args.host, args.read_port, args.send_port, args.history))
