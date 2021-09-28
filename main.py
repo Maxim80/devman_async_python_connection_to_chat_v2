fd2ff5153b42703d6955eb01f1e24378b71e3510
Maxim <maxim19802104@gmail.com>
2021-09-28 18:14:40 +0300

    Added new async function handle_connection(), added anyio.create_task_group.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from dotenv import load_dotenv
 from tkinter import messagebox
+from anyio import create_task_group
+from dotenv import load_dotenv
 import asyncio
 import aiofiles
 import async_timeout
@@ -9,6 +10,19 @@ import argparse
 import logging
 import os
 import json
+
+
+load_dotenv()
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('watchdog_logger')
+
+MINECHAT_HOST = 'minechat.dvmn.org'
+MINECHAT_READ_PORT = 5000
+MINECHAT_SEND_PORT = 5050
+MINECHAT_ACCESS_TOKEN = os.getenv('MINECHAT_ACCESS_TOKEN')
+HISTORY_FILEPATH = './minechat.history'
+CONNECTION_TIMEOUT = 3
 
 
 class InvalidToken(Exception):
@@ -32,44 +46,35 @@ async def authorize(host, port, token):
     return reader, writer, nickname
 
 
-async def send_msgs(host, port, token, sending_queue, status_updates_queue, watchdog_queue):
+async def send_msgs(host, port, token, sending_queue, status_updates_queue,
+        watchdog_queue):
     try:
         reader, writer, nickname = await authorize(host, port, token)
-        status_updates_queue.put_nowait(gui.NicknameReceived(nickname))
-        watchdog_queue.put_nowait('Authorization done.')
-        while True:
-            status_updates_queue.put_nowait(gui.SendingConnectionStateChanged.ESTABLISHED)
-            msg = await sending_queue.get()
-            writer.write(f'{msg}\n\n'.encode())
-            await writer.drain()
-            watchdog_queue.put_nowait('Message sent.')
-
     except InvalidToken:
         messagebox.showerror('Неверный токен', 'Проверьте токен. Сервер неузнал его.')
-        return True
-    finally:
-        writer.close()
-        await writer.wait_closed()
+        raise ConnectionError
+
+    status_updates_queue.put_nowait(gui.NicknameReceived(nickname))
+    watchdog_queue.put_nowait('Authorization done.')
+    status_updates_queue.put_nowait(gui.SendingConnectionStateChanged.ESTABLISHED)
+    while True:
+        msg = await sending_queue.get()
+        writer.write(f'{msg}\n\n'.encode())
+        await writer.drain()
+        watchdog_queue.put_nowait('Message sent.')
 
 
-async def read_msgs(host, port, messages_queue, status_updates_queue, save_queue, watchdog_queue):
-    try:
-        reader, writer = await asyncio.open_connection(host, port)
-        while True:
-            try:
-                status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.ESTABLISHED)
-                async with async_timeout.timeout(1):
-                    msg = await reader.readline()
-                messages_queue.put_nowait(msg.decode())
-                save_queue.put_nowait(msg.decode())
-                watchdog_queue.put_nowait('New message in chat.')
-            except asyncio.TimeoutError:
-                watchdog_queue.put_nowait('1s timeout is elapsed.')
-                status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.CLOSED)
+async def read_msgs(host, port, history_filepath, messages_queue,
+        status_updates_queue, save_queue, watchdog_queue):
+    await loading_messages_history(history_filepath, messages_queue)
 
-    finally:
-        writer.close()
-        await writer.wait_closed()
+    reader, writer = await asyncio.open_connection(host, port)
+    status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.ESTABLISHED)
+    while True:
+        msg = await reader.readline()
+        messages_queue.put_nowait(msg.decode())
+        save_queue.put_nowait(msg.decode())
+        watchdog_queue.put_nowait('New message in chat.')
 
 
 async def save_messages(filepath, save_queue):
@@ -81,12 +86,18 @@ async def save_messages(filepath, save_queue):
 
 async def watch_for_connection(watchdog_queue):
     while True:
-        msg = await watchdog_queue.get()
-        log_record = f'[{time.time()}] Connection is alive. {msg}'
-        logger.debug(log_record)
+        try:
+            async with async_timeout.timeout(CONNECTION_TIMEOUT):
+                msg = await watchdog_queue.get()
+        except asyncio.TimeoutError:
+            status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.CLOSED)
+            logger.debug(f'[{time.time()}] {CONNECTION_TIMEOUT}s timeout is elapsed.')
+            raise ConnectionError
+
+        logger.debug(f'[{time.time()}] Connection is alive. {msg}')
 
 
-async def loading_messages_history(filepath, queue):
+async def loading_messages_history(filepath, messages_queue):
     try:
         async with aiofiles.open(filepath, 'r') as f:
             msgs = await f.readlines()
@@ -94,42 +105,37 @@ async def loading_messages_history(filepath, queue):
         return
 
     if msgs:
-        [queue.put_nowait(msg) for msg in msgs]
+        [messages_queue.put_nowait(msg) for msg in msgs]
 
 
-async def main(host, read_port, send_port, history_filepath, token):
+async def handle_connection(messages_queue, sending_queue, status_updates_queue,
+        save_queue, watchdog_queue):
+    async with create_task_group() as task_group:
+        await task_group.spawn(read_msgs, MINECHAT_HOST, MINECHAT_READ_PORT,
+                HISTORY_FILEPATH, messages_queue, status_updates_queue,
+                save_queue, watchdog_queue)
+
+        await task_group.spawn(send_msgs, MINECHAT_HOST, MINECHAT_SEND_PORT,
+            MINECHAT_ACCESS_TOKEN, sending_queue, status_updates_queue, watchdog_queue)
+
+        await task_group.spawn(save_messages, HISTORY_FILEPATH, save_queue)
+
+        await task_group.spawn(watch_for_connection, watchdog_queue)
+
+
+async def main():
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     save_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
     watchdog_queue = asyncio.Queue()
 
-    await loading_messages_history(history_filepath, messages_queue)
-
     await asyncio.gather(
-        read_msgs(host, read_port, messages_queue, status_updates_queue, save_queue, watchdog_queue),
-        send_msgs(host, send_port, token, sending_queue, status_updates_queue, watchdog_queue),
-        save_messages(history_filepath, save_queue),
-        watch_for_connection(watchdog_queue),
+        handle_connection(messages_queue, sending_queue, status_updates_queue,
+            save_queue, watchdog_queue),
         gui.draw(messages_queue, sending_queue, status_updates_queue)
     )
 
 
 if __name__ == '__main__':
-    load_dotenv()
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--host', type=str, default='minechat.dvmn.org',
-        help='IP or URL hosts address for connection.')
-    parser.add_argument('--read_port', type=int, default=5000, help='Port to read.')
-    parser.add_argument('--send_port', type=int, default=5050, help='Port to send.')
-    parser.add_argument('--token', type=str, default=os.getenv('MINECHAT_ACCESS_TOKEN'),
-        help='Chat authorization token.')
-    parser.add_argument('--history', type=str, default='./minechat.history',
-        help='The path to the file with the message history.')
-    args = parser.parse_args()
-
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger('watchdog_logger')
-
-    asyncio.run(main(args.host, args.read_port, args.send_port, args.history, args.token))
+    asyncio.run(main())
